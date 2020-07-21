1b89111df165e5400897c199b9d5951fb60cd9fe
shruthy1992 <68539055+shruthy1992@users.noreply.github.com>
2020-07-21 16:57:19 +0530

    Update paramchecker.cpp

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -11,9 +11,9 @@ bool checkVitals(float vital, int lower, int upper)
 bool vitalsAreOk(float bpm, float spo2, float respRate) 
 { 
   bool isOk = false;
-  bool isOk = checkVitals(bpm,70,150);
-  bool isOk = isOk || checkVitals(spo2,80,100);
-  bool isOk =  isOk || checkVitals(respRate,30,60);
+  isOk = checkVitals(bpm,70,150);
+  isOk = isOk || checkVitals(spo2,80,100);
+  isOk =  isOk || checkVitals(respRate,30,60);
   if(isOk)
   {
     return true;
