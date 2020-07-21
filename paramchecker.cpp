6da273840f498cf922a1c201afd9e187a59c0a4e
shruthy1992 <68539055+shruthy1992@users.noreply.github.com>
2020-07-21 16:41:23 +0530

    Removed Duplication

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -9,11 +9,13 @@ bool checkVitals(float vital, int lower, int upper)
 }
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) 
-{
-  
+{ 
   bool isBpmOk = checkVitals(bpm,70,150);
   bool isSpo2Ok = checkVitals(spo2,80,100);
   bool isrespRateOk = checkVitals(respRate,30,60);
-  
-  return(isBpmOk & isSpo2Ok & isrespRateOk);
+  if(isBpmOk && isSpo2Ok && isrespRateOk)
+  {
+    return true;
+  }
+  return false;
 }
