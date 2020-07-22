5ea3e8f335331773db46a2f5045d320e8ea9a94e
shruthy1992 <68539055+shruthy1992@users.noreply.github.com>
2020-07-22 22:42:30 +0530

    Update testparamchecker.cpp

--- testparamchecker.cpp
+++ testparamchecker.cpp
@@ -3,15 +3,15 @@
 
 TEST(VitalsTest1, BPM) {
     std::vector<VitalValues> vitalVector = {{Vital::Bpm, 100,70, 150}, {Vital::Spo2, 90,80, 100}, {Vital::RespRate, 50,30, 60},{Vital::SugarLevel, 110,90, 120}};
-    ASSERT_EQ(vitalsAreOk(&vitalVector));
-    for(int i=0;i<vitalVector.size();i++
+    ASSERT_EQ(vitalsAreOk(vitalVector));
+    for(int i=0;i<vitalVector.size();i++)
     {
     TEST_OUT<<"Vital status:"<<vitalVector[i].vitalType<<":"<<vitalVector[i].vitalStatus<<"\n";
     }
 }
 TEST(VitalsTest2, SPO2) {
     std::vector<VitalValues> vitalVector = {{Vital::Bpm, 100,70, 150}, {Vital::Spo2, 40,80, 100}, {Vital::RespRate, 50,30, 60},{Vital::SugarLevel, 110,90, 120}};
-    ASSERT_EQ(vitalsAreOk(&vitalVector));
+    ASSERT_EQ(vitalsAreOk(vitalVector));
     
 }
 
