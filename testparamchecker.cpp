c5aefc02cb4c0140c1df2fe4f55cb57b9fd635b8
shruthy1992 <68539055+shruthy1992@users.noreply.github.com>
2020-07-22 19:38:49 +0530

    Update testparamchecker.cpp

--- testparamchecker.cpp
+++ testparamchecker.cpp
@@ -2,7 +2,7 @@
 #include <gtest/gtest.h>
 
 TEST(VitalsTest1, BPM) {
-    std::vector<VitalValues> vitalVector = {{Vital::Bpm, 60,70, 150}, {Vital::Spo2, 100,80, 101}, {Vital::RespRate, 50,30, 60}};
+    std::vector<VitalValues> vitalVector = {{Vital::Bpm, 100,70, 150}, {Vital::Spo2, 100,80, 101}, {Vital::RespRate, 50,30, 60}};
     ASSERT_EQ(true, vitalsAreOk(vitalVector));
 }
 TEST(VitalsTest2, SPO2) {
