4380b6256b040a9553109db4f3439d322fa9521d
shruthy1992 <68539055+shruthy1992@users.noreply.github.com>
2020-07-22 16:54:41 +0530

    Update testparamchecker.cpp

--- testparamchecker.cpp
+++ testparamchecker.cpp
@@ -1,36 +1,39 @@
 #include "paramchecker.h"
 #include <gtest/gtest.h>
+
 Vital vital;
 vector<Vital> vitals;
+
     vital.vitalValue =100;
     vital.lower = 70;
     vital.upper = 150;
-    vitals.pushback(vital)
+    vitals.pushback(vital);
     vital.vitalValue =100;
     vital.lower = 80;
     vital.upper = 100;
-    vitals.pushback(vital)
+    vitals.pushback(vital);
     vital.vitalValue = 80;
     vital.lower = 30;
     vital.upper = 60;
-    vitals.pushback(vital)
+    vitals.pushback(vital);
+
 TEST(VitalsTest, BPM) {
-    ASSERT_EQ(true, vitalsAreOk(vital));
+    ASSERT_EQ(true, vitalsAreOk(vitals));
 }
     vital.bpm =100;
     vital.lower = 70;
     vital.upper = 150;
-    vitals.pushback(vital)
+    vitals.pushback(vital);
     vital.spo2 =40;
     vital.lower = 80;
     vital.upper = 100;
-    vitals.pushback(vital)
+    vitals.pushback(vital);
     vital.resprate = 50;
     vital.lower = 30;
     vital.upper = 60;
-    vitals.pushback(vital)
+    vitals.pushback(vital);
 TEST(VitalsTest, SPO2) {
-    ASSERT_EQ(false, vitalsAreOk(vital));
+    ASSERT_EQ(false, vitalsAreOk(vitals));
 }
 
 int main(int argc, char **argv) {
