71886388beaed2a43abc13f660e4aa2789e2043c
shruthy1992 <68539055+shruthy1992@users.noreply.github.com>
2020-07-22 22:57:44 +0530

    Update paramchecker.cpp

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -9,12 +9,17 @@ bool checkVitals(float vital,int lower,int upper)
   return 1;
 }
 
-void vitalsAreOk(std::vector<VitalValues>& vitalVec) 
+bool vitalsAreOk(std::vector<VitalValues>& vitalVec) 
 {  
   int i;
+  bool isOk = true;
  for(i=0;i<vitalVec.size();i++)
  {
-  bool isOk = checkVitals(vitalVec[i].vitalValue, vitalVec[i].lower,vitalVec[i].upper);
-  vitalVec[i].vitalStatus= isOk; 
+    vitalVec[i].vitalStatus = checkVitals(vitalVec[i].vitalValue, vitalVec[i].lower,vitalVec[i].upper);
+  if(vitalVec[i].vitalStatus == false)
+  {
+   isOk = vitalVec[i].vitalStatus;
+  }
  }
+  return isOk;
 }
