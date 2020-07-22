eb8e20027b3fe3b6a934ad6d575e8484684cbef1
shruthy1992 <68539055+shruthy1992@users.noreply.github.com>
2020-07-22 23:52:19 +0530

    unit test case failure corrected
    unit test case was failing if enum value returned via local variable

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -11,14 +11,18 @@ bool checkVitals(float vital,int lower,int upper)
 
 Vital vitalsAreOk(std::vector<VitalValues>& vitalVec) 
 {  
-  //Vital vi = Vital::None;
+  int notOkIndex = 99;
  for(int i=0;i<vitalVec.size();i++)
  {
   vitalVec[i].vitalStatus = checkVitals(vitalVec[i].vitalValue, vitalVec[i].lower,vitalVec[i].upper);
-  //if(vitalVec[i].vitalStatus == false)
-  //{
-   //vi = vitalVec[i].vitalType;
-  //}
+  if(vitalVec[i].vitalStatus == false)
+  {
+   notOkIndex = i;
+  }
  }
-  return Vital::None;
+  if(index == 99)
+  {
+    return Vital::None;
+  }
+  return vitalVec[notOkIndex].vitalType;
 }
