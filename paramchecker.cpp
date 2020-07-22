75588f1527e680306055847ad96b46af46bcf868
shruthy1992 <68539055+shruthy1992@users.noreply.github.com>
2020-07-22 23:17:28 +0530

    removed unwanted var

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -9,13 +9,12 @@ bool checkVitals(float vital,int lower,int upper)
   return 1;
 }
 
-bool vitalsAreOk(std::vector<VitalValues>& vitalVec) 
+Vital vitalsAreOk(std::vector<VitalValues>& vitalVec) 
 {  
-  int i;
-  bool isOk = true;
- for(i=0;i<vitalVec.size();i++)
+  Vital isOk = Vital::None;
+ for(int i=0;i<vitalVec.size();i++)
  {
-    vitalVec[i].vitalStatus = checkVitals(vitalVec[i].vitalValue, vitalVec[i].lower,vitalVec[i].upper);
+  vitalVec[i].vitalStatus = checkVitals(vitalVec[i].vitalValue, vitalVec[i].lower,vitalVec[i].upper);
   if(vitalVec[i].vitalStatus == false)
   {
    isOk = vitalVec[i].vitalStatus;
