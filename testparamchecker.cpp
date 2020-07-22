3c03a458ed9395f05d17c8fdebf3441560cce827
shruthy1992 <68539055+shruthy1992@users.noreply.github.com>
2020-07-22 23:15:11 +0530

    Printing all the vital status

--- testparamchecker.cpp
+++ testparamchecker.cpp
@@ -3,16 +3,19 @@
 
 TEST(VitalsTest1, BPM) {
     std::vector<VitalValues> vitalVector = {{Vital::Bpm, 100,70, 150}, {Vital::Spo2, 90,80, 100}, {Vital::RespRate, 50,30, 60},{Vital::SugarLevel, 110,90, 120}};
-    ASSERT_EQ(true,vitalsAreOk(vitalVector));
+    ASSERT_EQ(Vital::None,vitalsAreOk(vitalVector));
     for(int i=0;i<vitalVector.size();i++)
     {
-    std::cout<<"Vital status:"<<vitalVector[i].vitalType<<":"<<vitalVector[i].vitalStatus<<"\n";
+      std::cout<<"Vital status:"<<vitalVector[i].vitalType<<":"<<vitalVector[i].vitalStatus<<"\n";
     }
 }
 TEST(VitalsTest2, SPO2) {
     std::vector<VitalValues> vitalVector = {{Vital::Bpm, 100,70, 150}, {Vital::Spo2, 40,80, 100}, {Vital::RespRate, 50,30, 60},{Vital::SugarLevel, 110,90, 120}};
-    ASSERT_EQ(false,vitalsAreOk(vitalVector));
-    
+    ASSERT_EQ(Vital::Spo2,vitalsAreOk(vitalVector));
+     for(int i=0;i<vitalVector.size();i++)
+    {
+      std::cout<<"Vital status:"<<vitalVector[i].vitalType<<":"<<vitalVector[i].vitalStatus<<"\n";
+    }   
 }
 
 int main(int argc, char **argv) {
