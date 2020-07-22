58341f9a2029aff223a4a8567cbf9ec17c343a86
shruthy1992 <68539055+shruthy1992@users.noreply.github.com>
2020-07-22 16:36:35 +0530

    Follows open close principle

--- testparamchecker.cpp
+++ testparamchecker.cpp
@@ -1,12 +1,36 @@
 #include "paramchecker.h"
 #include <gtest/gtest.h>
 
-TEST(VitalsTest, BPM) { 
-    ASSERT_EQ(true, vitalsAreOk(100, 100, 50));
+TEST(VitalsTest, BPM) {
+    vital.vitalValue =100;
+    vital.lower = 70;
+    vital.upper = 150;
+    vitals.pushback(vital)
+    vital.vitalValue =100;
+    vital.lower = 80;
+    vital.upper = 100;
+    vitals.pushback(vital)
+    vital.vitalValue = 80;
+    vital.lower = 30;
+    vital.upper = 60;
+    vitals.pushback(vital)
+    ASSERT_EQ(true, vitalsAreOk(vital));
 }
 
 TEST(VitalsTest, SPO2) {
-    ASSERT_EQ(false, vitalsAreOk(100, 40, 50));
+    vital.bpm =100;
+    vital.lower = 70;
+    vital.upper = 150;
+    vitals.pushback(vital)
+    vital.spo2 =40;
+    vital.lower = 80;
+    vital.upper = 100;
+    vitals.pushback(vital)
+    vital.resprate = 50;
+    vital.lower = 30;
+    vital.upper = 60;
+    vitals.pushback(vital)
+    ASSERT_EQ(false, vitalsAreOk(vital));
 }
 
 int main(int argc, char **argv) {
