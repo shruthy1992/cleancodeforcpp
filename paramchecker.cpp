9171e4538ab90ff65a8ee5447e07ff64daf4df74
shruthy1992 <68539055+shruthy1992@users.noreply.github.com>
2020-07-23 00:56:24 +0530

    Update paramchecker.cpp

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -17,7 +17,7 @@ Vital vitalsAreOk(std::vector<VitalValues>& vitalVec)
   vitalVec[i].vitalStatus = checkVitals(vitalVec[i].vitalValue, vitalVec[i].lower,vitalVec[i].upper);
   if(vitalVec[i].vitalStatus == false)
   {
-   vitalEnum =  vitalVec[0].vitalType;
+   vitalEnum =  vitalVec[i].vitalType;
   }
  }
   return vitalEnum;
